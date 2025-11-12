213d54e544a61070813182816a3c017a32eb39d3
Jorleonzu86 <jorleonzu16@gmail.com>
2025-11-12 14:49:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -218,6 +218,9 @@ def inicializar_bd():
 
 # --- EJECUCIÓN LOCAL ---
 if __name__ == "__main__":
+    import os
     inicializar_bd()
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    from flask import Flask
+    app.run(host="0.0.0.0", port=port)
+
