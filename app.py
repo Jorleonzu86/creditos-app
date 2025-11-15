4b72fff7b990bed63c61e51ef7b7d7e67a47f068
Jorleonzu86 <jorleonzu16@gmail.com>
2025-11-15 08:32:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,14 @@ from reportlab.lib.pagesizes import letter
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "cambia-esta-clave")
 
-# Render usa DATABASE_URL
-db_url = os.environ.get("DATABASE_URL", "sqlite:///creditos.db")
-if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+# ==== BASE DE DATOS: FORZAR SIEMPRE SQLITE (SIN POSTGRES) ==========
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, "creditos.db")
+
+# Usar SIEMPRE SQLite, ignorando DATABASE_URL de Render
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# ===================================================================
 
 db = SQLAlchemy(app)
 
